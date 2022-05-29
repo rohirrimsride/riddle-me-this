b3824069e490c4b19a26e1f932d35a20d0a3b642
Strider <dave.barnes23@gmail.com>
2022-05-29 01:06:13 -0500

    created quiz question card that cycles through questions/options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // build a javascript variable list to connect with html element id's
-questionCardEl = document.querySelector("#question-card");
+var questionCardEl = document.querySelector("#question-card");
+var countDownTimer = document.querySelector("#time")
 // create a customizable array of high scores
 var questionIdCounter = 0;
 var highScores = [];
@@ -9,6 +10,7 @@ var highScores = [];
 
 // starting page with start button
 var quizStartPage = function() {
+    // debugger;
     // creates quiz title
     var quizTitle = document.createElement("h2");
     quizTitle.className = ("quiz-title");
@@ -23,39 +25,103 @@ var quizStartPage = function() {
 
     // quiz start button
     var quizStartBtn = document.createElement("button");
-    quizStartBtn.className = "start-quiz"
-    quizStartBtn.textContent = "Start Quiz"
+    quizStartBtn.className = "start-quiz";
+    quizStartBtn.setAttribute("id", "start-quiz");
+    quizStartBtn.textContent = "Start Quiz";
     questionCardEl.appendChild(quizStartBtn);
 
-    // start quiz function
+    // click start button actions
+    quizStartBtn.onclick = function() {
+        questionCardEl.removeChild(quizTitle);
+        questionCardEl.removeChild(quizParameters);
+        questionCardEl.removeChild(quizStartBtn);
+        createQuestionEl();
+        startTimer();
+    };
+};   
 
-};
+
 
 // create html <li> elements using the choices class inside the <ul> element
 var createQuestionEl = function() {
     // creates a h3 element used to display the question
-    var questionEl = document.createElement("h3");
-    questionEl.className = "question";
-    questionEl.setAttribute("question-id", questionIdCounter);
+    
 
-    // create a ul element with li innerhtml element that holds the answer options
-    var answerOptionsEl = document.createElement ("ul");
-    answerOptionsEl.className = "options-holder";
-    answerOptionsEl.innerHTML = "li class"
+    // iterate through the quizQuestions array
+    
+    for (var i = 0; i < quizQuestions.length; i++) {
+        
+        var questionEl = document.createElement("h3");
+        questionEl.className = "question";
+        questionEl.textContent = (quizQuestions[i].question);
+        questionEl.setAttribute("id", "question-id");
+        questionCardEl.appendChild(questionEl);
+        console.log(questionEl);
+        
+        // debugger;
+        // create a div with an ol element with li element that holds the answer options
+        
+        var answerOptionsHolder = document.createElement("div");
+        answerOptionsHolder.className = "options-container";
+        questionCardEl.appendChild(answerOptionsHolder);
+
+            var answerOptionsEl = document.createElement("ol");
+            answerOptionsEl.className = "option-list";
+            answerOptionsHolder.appendChild(answerOptionsEl);
+
+                answerOptionsElA = document.createElement("li");
+                answerOptionsElA.className = "option";
+                answerOptionsElA.textContent = (quizQuestions[i].a);
+                answerOptionsElA.setAttribute("id", "a");
+                answerOptionsEl.appendChild(answerOptionsElA);
+
+                answerOptionsElB = document.createElement("li");
+                answerOptionsElB.className = "option";
+                answerOptionsElB.textContent = (quizQuestions[i].b);
+                answerOptionsElB.setAttribute("id", "b");
+                answerOptionsEl.appendChild(answerOptionsElB);
+
+                answerOptionsElC = document.createElement("li");
+                answerOptionsElC.className = "option";
+                answerOptionsElC.textContent = (quizQuestions[i].c);
+                answerOptionsElC.setAttribute("id", "c");
+                if (!quizQuestions[i].c) {    
+                } else {
+                    answerOptionsEl.appendChild(answerOptionsElC);
+                }                
+
+                answerOptionsElD = document.createElement("li");
+                answerOptionsElD.className = "option";
+                answerOptionsElD.textContent = (quizQuestions[i].d);
+                answerOptionsElD.setAttribute("id", "d");
+                if (!quizQuestions[i].d) {    
+                } else {
+                    answerOptionsEl.appendChild(answerOptionsElD);
+                }    
+        // debugger;
+        checkAnswer();
+
+       
+    };
 
 
 
 
+};
 
-}   
+var checkAnswer = function (event) {
+    var selectedOption = event.target;
+    console.log(selectedOption);
+debugger;
+}
 
 var saveScore = function() {
     localStorage.setItem("highScores", JSON.stringify(highScores));
 };
 
 var loadScore = function() {
-
-}
+    localStorage.getItem("highScores", JSON.parse(highScores));
+};
 
 // create a highscores display page
 
@@ -65,98 +131,101 @@ var loadScore = function() {
 var quizQuestions = [
     {
         question: "Which HTML element holds the JavaScript?",
-        options: {
+        
             a: "<link></link>",
             b: "<script></script>",
             c: "<js></js>",
-            d: "<html></html>"
-        },
+            d: "<html></html>",
+        
         answer: "b"
     },
     {
         question: "Which brackets are used to call a function?",
-        options: {
+        
             a: "{}",
             b: "<>",
             c: "[]",
-            d: "()"
-        },
+            d: "()",
+        
         answer: "d"
     },
     {
         question: "DOM stands for document object method",
-        options: {
+       
             a: "true",
-            b: "false"
-        },
+            b: "false",
+        
         answer: "b"
     },
     {
         question: "Which one is NOT a datatype?",
-        options: {
+
             a: "Boolean",
             b: "string",
             c: "variable",
-            d: "array"
-        },
+            d: "array",
+        
         answer: "c"
     },
     {
         question: "How do you add a comment in JavaScript?",
-        options: {
+    
             a: "<!-- This is a comment -->",
             b: "// this is a comment",
             c: "/* this is a comment */",
-            d: "both b and c"
-        },
+            d: "both b and c",
+        
         answer: "d"
     },
     {
-        question: "console.log(); logs a message to the browser console",
-        options: {
+        question: "console.log(); Logs a message to the browser console",
+
             a: "true",
-            b: "false"
-        },
+            b: "false",
+        
         answer: "a"
     },
     {
         question: "How is a Javascript variable declared?",
-        options: {
+
             a: "variable myVariable",
             b: "v = myVariable",
             c: "var myVariable",
-            d: "variable is myVariable"
-        },
+            d: "variable is myVariable",
+    
         answer: "c"
     },
     {
         question: "How do you create a function in JavaScript?",
-        options: {
+
             a: "myFunction ();",
             b: "function myFunction ()",
             c: "function = myFunction()",
-            d: "function (myFunction)"
-        },
+            d: "function (myFunction)",
+        
         answer: "b"
     },
     {
         question: "Is JavaScript case sensitive?",
-        options: {
+
             a: "true",
-            b: "false"
-        },
+            b: "false",
+        
         answer: "a"
     },
     {
         question: "What is the correct way to build a JavaScript array?",
-        options: {
+
             a: "var dogs = ['poodle', 'corgi', 'husky', 'mutt']",
             b: "var dogs = ('poodle', 'corgi', 'husky', 'mutt')",
             c: "var dogs = 'poodle', 'corgi', 'husky', 'mutt'",
-            d: "var dogs = poodle, corgi, husky, mutt"
-        },
+            d: "var dogs = poodle, corgi, husky, mutt",
+    
         answer: "a"
     }
 ];
+// add event listener in the parent element of createElement additions to listen for clicks on the list elements
+questionCardEl.addEventListener("click", checkAnswer);
 
+// initial quiz loading page
 quizStartPage ();
